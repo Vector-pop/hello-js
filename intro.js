953c79eb6db1b6ec409bc7c7fa76dca31c529ddb
EDP <victorayomidepopoola@gmail.com>
2026-01-17 19:07:25 +0100

    Initial commit: Little detial about myself

---
+++ intro.js
@@ -0,0 +1,4 @@
+const name = "Victor Popoola";
+age = 19;
+favourite_programming_language = "JavaScript";
+console.log(`My name is ${name}, I am ${age} years old and my favourite programming language is ${favourite_programming_language}.`);
